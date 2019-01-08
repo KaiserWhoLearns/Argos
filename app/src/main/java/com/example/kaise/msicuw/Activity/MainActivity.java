ff18d4506a926d4facdeee4862f99eb341cae450
JinghuaWang <johnjinghuawang@gmail.com>
2019-01-07 21:43:16 -0800

    TextToSpeech Demo is finished

---
+++ app/src/main/java/com/example/kaise/msicuw/Activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.kaise.msicuw.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.kaise.msicuw.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.TTSDemo_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, TTSDemoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
