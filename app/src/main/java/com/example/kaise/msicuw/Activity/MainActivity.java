9dbeef8f855f78198b74239b23841b7c6ed05e9d
JinghuaWang <johnjinghuawang@gmail.com>
2019-02-06 22:19:03 -0800

    Revert "Fix unable to instantiate bug"
    This reverts commit fc4318b95764b90caad05e9b4befd4a991d503b1.

--- app/src/main/java/com/example/kaise/msicuw/Activity/MainActivity.java
+++ app/src/main/java/com/example/kaise/msicuw/Activity/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.kaise.msicuw.Activity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,17 +11,17 @@ import com.example.kaise.msicuw.R;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle saveInstanceState){
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button)findViewById(R.id.TTSDemo_btn);
+        Button button = (Button) findViewById(R.id.TTSDemo_btn);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, TTSDemoActivity.class);
-                MainActivity.this.startActivity(intent);
+                startActivity(intent);
             }
         });
     }
