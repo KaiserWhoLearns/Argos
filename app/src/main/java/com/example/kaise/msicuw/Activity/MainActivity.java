fc4318b95764b90caad05e9b4befd4a991d503b1
JinghuaWang <johnjinghuawang@gmail.com>
2019-02-06 22:16:11 -0800

    Fix unable to instantiate bug
    I was stupid

--- app/src/main/java/com/example/kaise/msicuw/Activity/MainActivity.java
+++ app/src/main/java/com/example/kaise/msicuw/Activity/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.kaise.msicuw.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,17 +12,17 @@ import com.example.kaise.msicuw.R;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.TTSDemo_btn);
+        Button button = (Button)findViewById(R.id.TTSDemo_btn);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, TTSDemoActivity.class);
-                startActivity(intent);
+                MainActivity.this.startActivity(intent);
             }
         });
     }
